7c3d720fb7619c78cd943de5985cbb835a418721
syahba <ksyaahba@gmail.com>
2024-05-30 22:21:07 +0700

    create product controller

--- src/main/java/com/example/assignment_3/controller/ProductController.java
+++ src/main/java/com/example/assignment_3/controller/ProductController.java
@@ -1,5 +1,51 @@
 package com.example.assignment_3.controller;
 
-public class ProductController {
+import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.assignment_3.entity.Product;
+import com.example.assignment_3.repository.ProductRepository;
+
+@RestController
+@RequestMapping("products")
+
+public class ProductController {
+  private final Logger logger = LoggerFactory.getLogger(ProductController.class);
+
+  // inject bean
+  @Autowired
+  ProductRepository productRepository;
+
+  // get product api
+  @GetMapping("")
+  public List<Product> getProducts() {
+    List<Product> products = productRepository.findAll();
+    logger.info("get products success");
+
+    return products;
+  }
+
+  // create product api
+  @PostMapping("")
+  public Product createProduct(@RequestBody Product data) {
+    logger.info("save product success");
+    return productRepository.save(data);
+  }
+
+  // delete product api
+  @DeleteMapping("{id}")
+  public void deleteProduct(@PathVariable Long id) {
+    productRepository.deleteById(id);
+    logger.info("delete product success");
+  }
 }
